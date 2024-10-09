4ac17d4ddb9da2956cb861631756ee986cfd103a
REXTHEFOOL <nandoadit21@gmail.com>
2024-10-09 23:32:42 +0700

    menambah blog base design

--- main_screen/js/main.js
+++ main_screen/js/main.js
@@ -1,12 +1,12 @@
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
 
-next.addEventListener('click', function(){
+next.addEventListener('click', function () {
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').appendChild(items[0])
 })
 
-prev.addEventListener('click', function(){
+prev.addEventListener('click', function () {
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
@@ -18,15 +18,15 @@ let seeMoreButton = document.getElementById('.seeMore');
 let carousel = document.querySelector('.carousel');
 let listHTML = document.querySelector('.carousel .list');
 
-nextButton.oneclick = function(){
+nextButton.oneclick = function () {
     showSlider('next');
 }
-prevButton.oneclick = function(){
+prevButton.oneclick = function () {
     showSlider('prev');
 }
 const showSlider = (type) => {
     let items = document.querySelectorAll('.carousel .list .item');
-    if(type === 'next'){
+    if (type === 'next') {
         listHTML.appendChild(items[0]);
         carousel.classList.add('next');
     }
