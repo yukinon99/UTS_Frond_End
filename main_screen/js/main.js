6237e70a6cc03e2007f02bc47882f641e396a0b7
yukinon99 <edbert.535230033@stu.untar.ac.id>
2024-10-10 00:20:00 +0700

    menambahkan informasi

--- main_screen/js/main.js
+++ main_screen/js/main.js
@@ -1,33 +1,61 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+var nextBtn = document.querySelector('.next'),
+    prevBtn = document.querySelector('.prev'),
+    carousel = document.querySelector('.carousel'),
+    list = document.querySelector('.list'), 
+    item = document.querySelectorAll('.item'),
+    runningTime = document.querySelector('.carousel .timeRunning') 
 
-next.addEventListener('click', function () {
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
+let timeRunning = 3000 
+let timeAutoNext = 7000
 
-prev.addEventListener('click', function () {
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
-})
-
-let nextButton = document.getElementById('next');
-let prevButton = document.getElementById('prev');
-let backButton = document.getElementById('back');
-let seeMoreButton = document.getElementById('.seeMore');
-let carousel = document.querySelector('.carousel');
-let listHTML = document.querySelector('.carousel .list');
-
-nextButton.oneclick = function () {
-    showSlider('next');
+nextBtn.onclick = function(){
+    showSlider('next')
 }
-prevButton.oneclick = function () {
-    showSlider('prev');
+
+prevBtn.onclick = function(){
+    showSlider('prev')
 }
-const showSlider = (type) => {
-    let items = document.querySelectorAll('.carousel .list .item');
-    if (type === 'next') {
-        listHTML.appendChild(items[0]);
-        carousel.classList.add('next');
+
+let runTimeOut 
+
+let runNextAuto = setTimeout(() => {
+    nextBtn.click()
+}, timeAutoNext)
+
+
+function resetTimeAnimation() {
+    runningTime.style.animation = 'none'
+    runningTime.offsetHeight /* trigger reflow */
+    runningTime.style.animation = null 
+    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
+}
+
+
+function showSlider(type) {
+    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
+    if(type === 'next'){
+        list.appendChild(sliderItemsDom[0])
+        carousel.classList.add('next')
+    } else{
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
+        carousel.classList.add('prev')
     }
+
+    clearTimeout(runTimeOut)
+
+    runTimeOut = setTimeout( () => {
+        carousel.classList.remove('next')
+        carousel.classList.remove('prev')
+    }, timeRunning)
+
+
+    clearTimeout(runNextAuto)
+    runNextAuto = setTimeout(() => {
+        nextBtn.click()
+    }, timeAutoNext)
+
+    resetTimeAnimation() // Reset the running time animation
 }
+
+// Start the initial animation 
+resetTimeAnimation()
