ab197aa1e4d373cdd4f974c54baefa1481e144bb
yukinon99 <edbert.535230033@stu.untar.ac.id>
2024-10-09 18:49:32 +0700

    ganti gambar

--- main_screen/js/main.js
+++ main_screen/js/main.js
@@ -11,3 +11,23 @@ prev.addEventListener('click', function(){
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
 
+let nextButton = document.getElementById('next');
+let prevButton = document.getElementById('prev');
+let backButton = document.getElementById('back');
+let seeMoreButton = document.getElementById('.seeMore');
+let carousel = document.querySelector('.carousel');
+let listHTML = document.querySelector('.carousel .list');
+
+nextButton.oneclick = function(){
+    showSlider('next');
+}
+prevButton.oneclick = function(){
+    showSlider('prev');
+}
+const showSlider = (type) => {
+    let items = document.querySelectorAll('.carousel .list .item');
+    if(type === 'next'){
+        listHTML.appendChild(items[0]);
+        carousel.classList.add('next');
+    }
+}
