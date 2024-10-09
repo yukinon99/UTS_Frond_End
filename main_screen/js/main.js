31faa0e02d27cea473ff81a182525ac6748b0b5b
yukinon99 <edbert.535230033@stu.untar.ac.id>
2024-10-09 13:20:28 +0700

    merapikan semua ui dan file

--- main_screen/js/main.js
+++ main_screen/js/main.js
@@ -11,14 +11,3 @@ prev.addEventListener('click', function(){
     document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
 
-
-document.getElementById("search_button").onclick = function() {
-    // Mendapatkan nilai dari input
-    var query = document.getElementById("search-input").value;
-    
-    // Tentukan URL tujuan, bisa disesuaikan sesuai kebutuhan
-    var targetUrl = "search.html" + encodeURIComponent(query);
-    
-    // Mengalihkan ke URL yang dituju
-    window.location.href = targetUrl;
-};
