e5c3a21dec4ad0f0d95fd7270674b3d5f0ca74ba
REXTHEFOOL <nandoadit21@gmail.com>
2024-10-11 02:01:44 +0700

    Menambah fase ketiga blog

---
+++ main_screen/js/blog.js
@@ -0,0 +1,43 @@
+const likeBtn = document.querySelector('.like-btn');
+const dislikeBtn = document.querySelector('.dislike-btn');
+const likeCountSpan = document.querySelector('.like-count');
+const dislikeCountSpan = document.querySelector('.dislike-count');
+
+let likeCount = 0;
+let dislikeCount = 0;
+
+likeBtn.addEventListener('click', () => {
+    if (likeBtn.classList.contains('active')) {
+
+        likeBtn.classList.remove('active');
+        likeCount--;
+    } else {
+        likeBtn.classList.add('active');
+        dislikeBtn.classList.remove('active');
+        likeCount++;
+        if (dislikeCount > 0) {
+            dislikeCount--;
+        }
+    }
+    likeCountSpan.textContent = likeCount;
+    dislikeCountSpan.textContent = dislikeCount;
+});
+
+dislikeBtn.addEventListener('click', () => {
+    if (dislikeBtn.classList.contains('active')) {
+
+        dislikeBtn.classList.remove('active');
+        dislikeCount--;
+    } else {
+
+        dislikeBtn.classList.add('active');
+        likeBtn.classList.remove('active');
+        dislikeCount++;
+        if (likeCount > 0) {
+            likeCount--;
+        }
+    }
+
+    likeCountSpan.textContent = dislikeCount;
+    dislikeCountSpan.textContent = dislikeCount;
+});
